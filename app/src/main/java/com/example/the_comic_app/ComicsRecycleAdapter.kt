b12ff8b9ddc59bd663d60d4ad5abab521b796235
Noccis <89582846+Noccis@users.noreply.github.com>
2022-03-28 21:53:41 +0200

    minor design fix

--- app/src/main/java/com/example/the_comic_app/ComicsRecycleAdapter.kt
+++ app/src/main/java/com/example/the_comic_app/ComicsRecycleAdapter.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
@@ -24,9 +25,14 @@ class ComicsRecycleAdapter(val context: Context, val comics: List<ComicStrip> ) 
         // Current comic
         val comic = comics[position]
         // Load image url into imageView using Glide
-        Glide.with(context)
-            .load(comic.img)
-            .into(holder.comicImage)
+        if(comic.img != null) {
+            Glide.with(context)
+                .load(comic.img)
+                .into(holder.comicImage)
+        }
+        // Set comic id and description
+        holder.comicId.text = comic.id.toString()
+        holder.comicDescription.text = comic.description
     }
 
     override fun getItemCount() = comics.size
@@ -34,6 +40,8 @@ class ComicsRecycleAdapter(val context: Context, val comics: List<ComicStrip> ) 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         // Locate the views in the list_item
         val comicImage = itemView.findViewById<ImageView>(R.id.comicImageView)
+        val comicId = itemView.findViewById<TextView>(R.id.comicIdTextView)
+        val comicDescription = itemView.findViewById<TextView>(R.id.descriptionTextView)
     }
 /*
 fun loadImage(url:String, view: ImageView, context: Context) {

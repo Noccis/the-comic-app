8320b5d161ef51c47d6fb42e6f5fc0a75728e88d
Noccis <89582846+Noccis@users.noreply.github.com>
2022-03-29 12:50:52 +0200

    added explanation textview and button to toggle visibility

--- app/src/main/java/com/example/the_comic_app/ComicsRecycleAdapter.kt
+++ app/src/main/java/com/example/the_comic_app/ComicsRecycleAdapter.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -24,6 +25,9 @@ class ComicsRecycleAdapter(val context: Context, val comics: List<ComicStrip> ) 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         // Current comic
         val comic = comics[position]
+
+        // Bool to keep track on explanation visibility
+        var explanationIsVisible = false
         // Load image url into imageView using Glide
         if(comic.img != null) {
             Glide.with(context)
@@ -33,6 +37,17 @@ class ComicsRecycleAdapter(val context: Context, val comics: List<ComicStrip> ) 
         // Set comic id and description
         holder.comicId.text = comic.id.toString()
         holder.comicDescription.text = comic.description
+        holder.comicExplanation.visibility = View.INVISIBLE
+        holder.toggleExplanationbutton.setOnClickListener {
+            if (!explanationIsVisible) {
+                holder.comicExplanation.visibility = View.VISIBLE
+                explanationIsVisible = true
+                Log.d("Dodo", "Explanation should now be visible")
+            }else{
+                holder.comicExplanation.visibility = View.INVISIBLE
+                explanationIsVisible = false
+            }
+        }
     }
 
     override fun getItemCount() = comics.size
@@ -42,6 +57,8 @@ class ComicsRecycleAdapter(val context: Context, val comics: List<ComicStrip> ) 
         val comicImage = itemView.findViewById<ImageView>(R.id.comicImageView)
         val comicId = itemView.findViewById<TextView>(R.id.comicIdTextView)
         val comicDescription = itemView.findViewById<TextView>(R.id.descriptionTextView)
+        val toggleExplanationbutton = itemView.findViewById<Button>(R.id.toggleExplanationbutton)
+        var comicExplanation = itemView.findViewById<TextView>(R.id.explanationTextView)
     }
 /*
 fun loadImage(url:String, view: ImageView, context: Context) {
